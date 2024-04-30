6c3a7fdc2d9c8fc0310549f6746282c89d0b0efd
Vashu Haritasya <vashusharma246@gmail.com>
2024-04-30 20:34:16 +0530

    Added Media, Orders, Products And ProductFile

--- src/lib/validators/account-credentials-validators.ts
+++ src/lib/validators/account-credentials-validators.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const AuthcredentialsValidator = z.object({
   email: z.string().email(),
   password: z.string().min(8, {
-    message: "Password should be at least 8 characters.",
+    message: "Password should be at least 8 characters long.",
   }),
 });
 
