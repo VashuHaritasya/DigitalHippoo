894766e4e71357b13e58407a69dc296a98680ef1
Vashu Haritasya <vashusharma246@gmail.com>
2024-05-05 20:44:20 +0530

    lot of stuff

--- src/collections/Products/Products.ts
+++ src/collections/Products/Products.ts
@@ -1,5 +1,17 @@
+import {
+  AfterChangeHook,
+  BeforeChangeHook,
+} from "payload/dist/collections/config/types";
 import { PRODUCT_CATEGORIES } from "../../config";
-import { CollectionConfig } from "payload/types";
+import { Access, CollectionConfig } from "payload/types";
+import { Product, User } from "../../payload-types";
+import { stripe } from "../../lib/stripe";
+
+const addUser: BeforeChangeHook<Product> = async ({ req, data }) => {
+  const user = req.user;
+
+  return { ...data, user: user.id };
+};
 
 export const Products: CollectionConfig = {
   slug: "products",
@@ -7,6 +19,48 @@ export const Products: CollectionConfig = {
     useAsTitle: "name",
   },
   access: {},
+  hooks: {
+    beforeChange: [
+      addUser,
+      async (args) => {
+        if (args.operation === "create") {
+          const data = args.data as Product;
+
+          const createdProduct = await stripe.products.create({
+            name: data.name,
+            default_price_data: {
+              currency: "USD",
+              //@ts-ignore
+              unit_amount: Math.round(data.price * 100),
+            },
+          });
+
+          const updated: Product = {
+            ...data,
+            stripeId: createdProduct.id,
+            priceId: createdProduct.default_price as string,
+          };
+
+          return updated;
+        } else if (args.operation === "update") {
+          const data = args.data as Product;
+
+          const updatedProduct = await stripe.products.update(data.stripeId!, {
+            name: data.name,
+            default_price: data.priceId!,
+          });
+
+          const updated: Product = {
+            ...data,
+            stripeId: updatedProduct.id,
+            priceId: updatedProduct.default_price as string,
+          };
+
+          return updated;
+        }
+      },
+    ],
+  },
   fields: [
     {
       name: "user",
@@ -27,7 +81,7 @@ export const Products: CollectionConfig = {
     {
       name: "description",
       type: "textarea",
-      label: "Products details",
+      label: "Product details",
     },
     {
       name: "price",
@@ -44,7 +98,6 @@ export const Products: CollectionConfig = {
       options: PRODUCT_CATEGORIES.map(({ label, value }) => ({ label, value })),
       required: true,
     },
-
     {
       name: "product_files",
       label: "Product file(s)",
@@ -55,7 +108,7 @@ export const Products: CollectionConfig = {
     },
     {
       name: "approvedForSale",
-      label: "Product status",
+      label: "Product Status",
       type: "select",
       defaultValue: "pending",
       access: {
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -3,9 +3,11 @@ import { authRouter } from "./authRouter";
 import { publicProcedure, router } from "./trpc";
 import { QueryValidator } from "../lib/validators/query-validator";
 import { getPayloadClient } from "../get-payload";
+import { paymentRouter } from "./payment-router";
 
 export const appRouter = router({
   auth: authRouter,
+  payment: paymentRouter,
 
   getInfiniteProducts: publicProcedure
     .input(
